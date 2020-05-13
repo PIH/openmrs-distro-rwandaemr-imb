fa52b9da4e67257ef1b18fa9cd1cce91a0c3880e
James Mbabazi <jmbabazi@pih.org>
2020-05-13 15:45:04 -0400

    Modifications to the scripts

--- rwandaemr-installer/src/main/resources/scripts/install.sh
+++ rwandaemr-installer/src/main/resources/scripts/install.sh
@@ -3,37 +3,29 @@
 ## load variables from the vars.txt file
 source vars.txt
 
-create_target_dir(){
-    if [ "$env_type" == "demo" -o "$env_type" == "prod" -o "$env_type" == "test" ]; then
-      mkdir -p /opt/${parent_dir}/distribution
-    else
-      mkdir -p ${parent_dir}
-      mkdir -p ${parent_dir}/distribution
-    fi
-}
-
-create-service-user(){
-	./create-service-user.sh ${parent_dir}
+create_service_user(){
+	./create-service-user.sh
 }
 
 download_distribution(){
-if [ "$env_type" == "demo" -o "$env_type" == "prod" -o "$env_type" == "test" ]; then
 	./download-distribution.sh
-else
-  ./download-distribution.sh
-fi
 }
 
 change_ownership() {
-if [ "$env_type" == "demo" -o "$env_type" == "prod" -o "$env_type" == "test" ]; then
 	chown -R ${username}:${username} /opt/${parent_dir}
 	chmod 0700 /opt/${parent_dir}
-else
-  echo " "
-fi
 }
 
-create_target_dir
-create-service-user
-download_distribution
-change_ownership
+if [ "$env_type" == "demo" -o "$env_type" == "prod" -o "$env_type" == "test" ]; then
+  mkdir -p /opt/${parent_dir}
+  mkdir -p /opt/${parent_dir}/distribution
+  create_target_dir
+  create_service_user
+  download_distribution
+  change_ownership
+else
+  mkdir -p ${parent_dir}
+  mkdir -p ${parent_dir}/distribution
+  create_target_dir
+  download_distribution
+fi

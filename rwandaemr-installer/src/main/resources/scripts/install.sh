24454c35e8976b2bce39d7a5dbac05d2d8d4005f
James Mbabazi <jmbabazi@pih.org>
2020-05-14 15:20:18 -0400

    Modifying scripts

--- rwandaemr-installer/src/main/resources/scripts/install.sh
+++ rwandaemr-installer/src/main/resources/scripts/install.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-## load variables from the vars.txt file
-source vars.txt
+## load variables from the env file
+source vars
 
-create_service_user(){
-	./create-service-user.sh
+create_base_dir(){
+  mkdir -p ${BASE_DIR}
 }
 
-download_distribution(){
-	./download-distribution.sh
+download_maven_distribution(){
+    ARTIFACT_ID=${ARTIFACT_ID1}
+    CLASSIFIER=${CLASSIFIER1}
+    TARGET_DIR=${BASE_DIR}/${CLASSIFIER}
+    create_base_dir
+    mkdir -p ${TARGET_DIR}
+	  ./download-maven-artifacts ${ARTIFACT_ID} ${VERSION} ${TARGET_DIR} ${CLASSIFIER}
+	  mv $(pwd)/$TARGET_DIR/${ARTIFACT_ID1}-${VERSION}/* $TARGET_DIR
+	  rm -rf $(pwd)/$TARGET_DIR/${ARTIFACT_ID1}-${VERSION}
 }
 
-change_ownership() {
-	chown -R ${username}:${username} /opt/${parent_dir}
-	chmod 0700 /opt/${parent_dir}
+download_maven_installer(){
+	  CLASSIFIER=${CLASSIFIER2}
+	  TARGET_DIR=${BASE_DIR}/${CLASSIFIER}
+	  create_base_dir
+	  mkdir -p ${TARGET_DIR}
+	  ./download-maven-artifacts ${ARTIFACT_ID2} ${INSTALLER_VERSION} ${TARGET_DIR} ${CLASSIFIER}
+	  mv $(pwd)/$TARGET_DIR/${ARTIFACT_ID2}-${INSTALLER_VERSION}/* $TARGET_DIR
+	  rm -rf $(pwd)/$TARGET_DIR/${ARTIFACT_ID2}-${INSTALLER_VERSION}
 }
 
-if [ "$env_type" == "demo" -o "$env_type" == "prod" -o "$env_type" == "test"  ]; then
-  mkdir -p /opt/${parent_dir}
-  mkdir -p /opt/${parent_dir}/distribution
-  create_service_user
-  download_distribution
-  change_ownership
-else
-  mkdir -p ${parent_dir}
-  mkdir -p ${parent_dir}/distribution
-  download_distribution
-fi
+download_maven_distribution
+download_maven_installer
